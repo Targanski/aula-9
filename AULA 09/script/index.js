9372cb59cc3011b0d8051f6ad6a8ccf8e986edab
Targanski <101233340+Targanski@users.noreply.github.com>
2022-05-23 23:44:10 -0300

    Add files via upload

---
+++ AULA 09/script/index.js
@@ -0,0 +1,11 @@
+$("#btnLogin").click(
+    function(){
+
+        
+        console.log("o valor do campo email é.");
+        console.log(
+            document.getElementById('inputemail').value
+            );
+             
+    }
+)
